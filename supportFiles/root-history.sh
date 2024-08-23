31713a1ebdd46b8550edaf3ceb2e53a7c1b7ce8b
romandobra <67537080+romandobra@users.noreply.github.com>
2024-08-23 12:15:29 +0200

    fix

--- supportFiles/root-history.sh
+++ supportFiles/root-history.sh
@@ -1,2 +1,1 @@
-. <(wget -O - https://raw.githubusercontent.com/romandobra/lessbian/main/install.sh)
-. <(wget -O - https://raw.githubusercontent.com/romandobra/lessbian/main/build-base.sh)
+. <(wget -O - https://raw.githubusercontent.com/romandobra/lessbian/main/tester/fresh-install.sh)
